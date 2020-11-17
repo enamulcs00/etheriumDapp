a560e06208b4deb68693271e95cfee76b08e3893
Administrator <admin@mobiloitte.com>
2020-11-17 10:54:33 +0000

    Sonar added

---
+++ sonarqube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/home/administrator/sonar-scanner-4.3.0.2102/bin/sonar-scanner \
+  -Dsonar.projectKey=company-needs-ethereum-dapp-smartcontract-20073725-angular-ph-enamulhaque \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://172.16.0.220:9000 \
+  -Dsonar.login=32cf9325138f97409198a1a1373cd27f6c9cdded \
+  -Dsonar.exclusions=dist/**,node_modules/**
