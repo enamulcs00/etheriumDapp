69ae6dbce1eff9630562c6d2d3357df4b18bc976
Enamul haque <enamulcs008@gmail.com>
2020-11-21 12:08:06 +0530

    User-wallet re-balance feature added

--- src/app/pages/user-wallet-rebalance/user-wallet-rebalance.component.ts
+++ src/app/pages/user-wallet-rebalance/user-wallet-rebalance.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ShareableService } from './../../shareable.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-user-wallet-rebalance',
   templateUrl: './user-wallet-rebalance.component.html',
   styleUrls: ['./user-wallet-rebalance.component.css']
 })
-export class UserWalletRebalanceComponent implements OnInit {
+export class UserWalletRebalanceComponent implements OnInit ,OnDestroy{
 
-  constructor() { }
+  constructor(private service:ShareableService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.service.IshomePage.next(false);
+    this.service.IsUserWallet.next(true);
   }
-
+ngOnDestroy(){
+  this.service.IsUserWallet.next(false);
+}
 }
